902d252e0316a20cf16f5095feb68bb0bebc8f7e
xdvriend <xeno.dv@pandora.be>
2023-04-17 15:35:16 +0000

    fix df

--- code/merge.py
+++ code/merge.py
@@ -23,11 +23,13 @@ def merge(name, directory):
 
     total_df = total_df.sort_values(['t_val'], ascending=[True])
     total_df = total_df.reset_index(drop=True)
-    total_df.to_json(name, orient="columns")
 
     total_df["particles"] = total_df.apply(lambda row: fromList(row["particles"]), axis=1)
     total_df["energy"] = total_df.apply(lambda row: fromList(row["energy"]), axis=1)
     total_df["t_val"] = total_df.apply(lambda row: fromList(row["t_val"]), axis=1)
+    
+    total_df.to_json(name, orient="columns")
+
 
     print(total_df)
 
